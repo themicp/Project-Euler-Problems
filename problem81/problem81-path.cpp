81550f03519edce38557a7921200984f41b5b5d6
Themis Papameletiou <themicp@gmail.com>
2012-01-29 12:46:43 +0200

    Problem 81 - Path

--- problem81/problem81-path.cpp
+++ problem81/problem81-path.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+const int LEFT = 0;
+const int UP = 1;
 
 inline int min( int a, int b ) {
     if ( a < b ) {
@@ -13,7 +15,7 @@ inline int min( int a, int b ) {
 
 FILE *in = fopen( "problem81.in", "r" ), *out = fopen( "problem81.out", "w" );
 int i, j, W[ 80 ][ 80 ], a, path[ 80 ][ 80 ], N;
-stack< int > path;
+stack< int > p;
 
 int main() {
     fscanf( in, "%i", &N );
@@ -29,27 +31,47 @@ int main() {
                 }
                 else {
                     W[ 0 ][ j ] = W[ 0 ][ j - 1 ] + a;
-                    path[ 0 ][ j ] = 0;
+                    path[ 0 ][ j ] = LEFT;
                 }
             }
             else {
                 if ( j == 0 ) {
                     W[ i ][ 0 ] = W[ i - 1 ][ 0 ] + a;
-                    path[ i ][ 0 ] = 1;
+                    path[ i ][ 0 ] = UP;
                 }
                 else {
                     W[ i ][ j ] = min( W[ i - 1 ][ j ], W[ i ][ j - 1 ] ) + a;
                     if ( W[ i ][ j ] - a == W[ i - 1 ][ j ] ) {
-                        path[ i ][ j ] = 1;
+                        path[ i ][ j ] = UP;
                     }
                     else {
-                        path[ i ][ j ] = 0;
+                        path[ i ][ j ] = LEFT;
                     }
                 }
             }
         }
     }
 
-    printf( "%i\n", W[ N - 1 ][ N - 1 ] );
+    j = N - 1;
+    i = N - 1;
+    while ( i > 0 || j > 0 ) {
+       p.push( path[ i ][ j ] ); 
+       if ( path[ i ][ j ] == LEFT ) {
+           --j;
+           continue;
+       }
+       --i;
+    }
+    while ( !p.empty() ) {
+        if ( p.top() == UP ) {
+            printf( "↓ " );
+        }
+        else {
+            printf( "→ " );
+        }
+        p.pop();
+    }
+
+    printf( "\n%i\n", W[ N - 1 ][ N - 1 ] );
     return 0;
 }
