17317d7c4a4a105e57bdace5f29f39f4cd6019e5
themicp <themicp@gmail.com>
2014-12-07 18:52:50 +0200

    Refactor of the solution for Problem #15

--- problem15/problem15.cpp
+++ problem15/problem15.cpp
@@ -1,28 +1,45 @@
 #include <cstdio>
-#include <algorithm>
+#include <cstdlib>
 
 using namespace std;
 
-const int N = 2;
+#define N 20
 
-int i, A[ 2*N ];
+long int** C, i, j;
+
+long int CountRoutes( int x, int y ) {
+    if ( x == 0 && y == 0 ) {
+        return 1;
+    }
+    long int cost = 0;
+    if ( y > 0 ) {
+        if ( C[ x ][ y - 1 ] == -1 ) {
+            C[ x ][ y - 1 ] = CountRoutes( x, y - 1 );
+        }
+
+        cost += C[ x ][ y - 1 ];
+    }
+    if ( x > 0 ) {
+        if ( C[ x - 1 ][ y ] == -1 ) {
+            C[ x - 1 ][ y ] = CountRoutes( x - 1, y );
+        }
+
+        cost += C[ x - 1 ][ y ];
+    }
+
+    return cost;
+}
 
 int main() {
-    for ( i = 0; i < N; ++i ) {
-        A[ i ] = 0;
-        A[ i + N ] = 1;
-    }
+    C = ( long int** )malloc( ( N + 1 ) * sizeof( long int* ) );
+    for ( i = 0; i <= N; ++i ) {
+        C[ i ] = ( long int* )malloc( ( N + 1 ) * sizeof( long int ) );
 
-    i = 1;
-    while ( next_permutation( A, A + 2 * N ) ) {
-        for ( i = 0; i < 2 * N; ++i ) {
-            printf( "%i ", A[ i ] );
+        for ( j = 0; j <= N; ++j ) {
+            C[ i ][ j ] = -1;
         }
-        printf( "\n" );
-        ++i;
     }
 
-    printf( "Solution: %i\n", i );
-
+    printf( "Cost: %li\n", CountRoutes( N, N ) );
     return 0;
 }
